19d275d3c8038744a84816ddf3deb00f32e6b674
Andrew <K1304716@kingston.ac.uk>
2016-02-16 18:01:34 +0000

    First Commit, not tested movement on phone, collisions between marble and holes work

---
+++ Assets/Marble.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Marble : MonoBehaviour {
+
+	public float speed = 500;
+	public float sensH = 10;
+	public float sensV = 10;
+	Rigidbody2D rigidBody;
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+
+
+	void FixedUpdate()
+	{
+		float moveHor = Mathf.Clamp (Input.acceleration.x * sensH, -1, 1);
+		float moveVer = Mathf.Clamp (Input.acceleration.y * sensV, -1, 1);
+		rigidBody.AddForce (new Vector3 (moveHor, 0, moveVer) * speed * Time.deltaTime);
+	}
+	// Update is called once per frame
+	/*void Update () {
+	
+	
+	if(Input.GetKey(KeyCode.W))
+		{
+			transform.position += transform.up * 50.0f * Time.deltaTime;	
+		}
+	if(Input.GetKey(KeyCode.S))
+		{
+			transform.position -= transform.up * 50.0f * Time.deltaTime;	
+		}
+	if(Input.GetKey(KeyCode.A))
+		{
+			transform.position -= transform.right * 50.0f * Time.deltaTime;	
+		}
+	if(Input.GetKey(KeyCode.D))
+		{
+			transform.position += transform.right * 50.0f * Time.deltaTime;	
+		}
+
+	}*/
+
+
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+
+		if (coll.tag == "Hole") {
+			transform.position = new Vector2(-67,30);
+		}
+	}
+
+
+}
